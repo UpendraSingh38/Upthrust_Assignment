1849f442ffbca2dc6c1f906b560cceac713df805
Upendra Singh <upendrasingh9989@gmail.com>
2025-09-19 06:41:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,9 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+origin:"https://upthrust.onrender.com"  
+));
 app.use(express.json());
 
 const PORT = process.env.PORT || 4000;
